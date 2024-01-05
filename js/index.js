ae2e93ad2aa1f1a7cc8e1e1104abb8171c86fd6b
Angus Fretwell <angusfretwell@me.com>
2024-01-05 21:52:38 +1100

    use stark base theme, improve afform and searchkit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 (function ($, _) {
-  CRM.menubar.removeToggleButton();
-  CRM.menubar.position = "below-cms-menu";
-  $("body").removeClass("crm-menubar-over-cms-menu");
-  $("body").addClass("crm-menubar-below-cms-menu");
+  if (CRM.menubar) {
+    CRM.menubar.removeToggleButton();
+    CRM.menubar.position = "below-cms-menu";
+    $("body").removeClass("crm-menubar-over-cms-menu");
+    $("body").addClass("crm-menubar-below-cms-menu");
+  }
 })(CRM.$, CRM._);
